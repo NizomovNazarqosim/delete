157a17a6d808b18b540ce502800b3169f40b11eb
NizomovNazarqosim <nazarqosimnizomov@gmail.com>
2022-06-11 14:08:54 +0500

    testtttt

---
+++ script.js
@@ -0,0 +1,7 @@
+function searchProduct(){
+    // DOM
+ const input = document.getElementById("filter").value.toUpperCase();
+ const cardContainer = document.getElementById("card-list");
+
+ const cards = cardContainer.getElementsByClassName("card");
+}
